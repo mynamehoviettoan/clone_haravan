46ddd394ebc5dd9714f88cdb6e329e86b4972ed3
mynamehoviettoan <mynamehoviettoan@gmail.com>
2025-03-11 08:33:49 +0700

    main

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        customOrange: "#FE9705",
+      }
+    },
+  },
+  plugins: [],
+}
+
